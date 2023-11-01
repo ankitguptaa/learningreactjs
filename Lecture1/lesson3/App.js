c50032eeb66b83373811767afaeacf03145b5afd
Ankit Gupta10 <ankit.gupta10@ibm.com>
2023-11-01 16:19:39 +0530

    Lecture 1 Files added.

---
+++ Lecture1/lesson3/App.js
@@ -0,0 +1,17 @@
+const root = ReactDOM.createRoot(document.getElementById('root'));
+const htmlBody = React.createElement("div", { id: "parent" }, [
+    React.createElement("div", { id: "child" },
+        [
+            React.createElement("h1", {}, "I am an h1 tag"),
+            React.createElement("h2", {}, "I am an h2 tag")
+        ]
+    ),
+    React.createElement("div", { id: "child2" },
+    [
+        React.createElement("h1", {}, "I am an h1 tag"),
+        React.createElement("h2", {}, "I am an h2 tag")
+    ]
+),
+]);
+root.render(htmlBody);
+
